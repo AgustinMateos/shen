2a83c509f90542ba82b741f574d99ff3f7ee4d33
agustin mateos <agustinrotondomateos@gmail.com>
2022-11-21 05:04:45 -0300

    shen home

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const productos = [
+    {id:1 , name:'CT 15' , description:'Canela , trocitos de manzana , miel y naranja.' , weight:'50gr' },
+    {id:2 , name:'MM 10' , description:'Narnaja , rosa mosqueta y anis.' ,weight:'50gr' }
+    {id:3 , name:'LS 07' , description:'Limon y miel.' ,weight:'50gr' }
+]
+
+
+
+
+const 
